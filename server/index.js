6bf78dc0dfdf6586085696eee46ade033c8dc94a
manasapapunuka <126868703+manasapapunuka@users.noreply.github.com>
2024-05-02 13:10:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,14 +13,6 @@ app.use(cors(
   }
 ));
 
-app.use((request,response,next)=>{
-  response.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  response.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
 
 
 
